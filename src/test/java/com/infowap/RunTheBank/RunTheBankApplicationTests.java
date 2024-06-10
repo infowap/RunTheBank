4c9cd2955e5395d04e94715cef0bffc312e6e41c
infowap <adaomasso@gmail.com>
2024-06-09 22:29:57 -0300

    Initial commit

---
+++ src/test/java/com/infowap/RunTheBank/RunTheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infowap.RunTheBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
