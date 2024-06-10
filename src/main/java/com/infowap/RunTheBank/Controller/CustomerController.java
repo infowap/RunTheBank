37a1c957983406f1574ce4f7a2efaec57968030e
infowap <adaomasso@gmail.com>
2024-06-10 02:43:40 -0300

    Create class CustomerController

---
+++ src/main/java/com/infowap/RunTheBank/Controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.infowap.RunTheBank.Controller;
+
+import com.infowap.RunTheBank.Exceptions.AccountServiceException;
+import com.infowap.RunTheBank.Model.Customer;
+import com.infowap.RunTheBank.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer createdCustomer = customerService.createCustomer(customer);
+        return ResponseEntity.ok(createdCustomer);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
+        Customer customer = customerService.findById(id)
+                .orElseThrow(() -> new AccountServiceException("Customer not found"));
+        return ResponseEntity.ok(customer);
+    }
+}
